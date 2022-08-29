abdfeb2048ba376b9d86e0b3c38d38a7ac94c5e0
Fabricio-Guima <fsgkof@gmail.com>
2022-08-29 20:09:12 -0300

    paginacao

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import store from './store'
 import '@/plugins/vee-validate'
 import '@/plugins/axios'
 import Toasted from 'vue-toasted'
+import VuePaginate from 'vue-paginate'
 
 const options = {
   duration: 3000,
 }
+
 Vue.use(Toasted, options)
+Vue.use(VuePaginate)
 
 Vue.config.productionTip = false
 
--- src/views/collaborators/Index.vue
+++ src/views/collaborators/Index.vue
@@ -149,8 +149,16 @@
                           </th>
                         </tr>
                       </thead>
-                      <tbody class="list fs-base">
-                        <tr v-for="(user, index) in users" :key="index">
+                      <!-- Pagination -->
+                      <paginate
+                        ref="users"
+                        tag="tbody"
+                        name="users"
+                        :list="users"
+                        :per="perPage"
+                        class="list fs-base"
+                      >
+                        <tr v-for="(user, index) in paginated('users')" :key="index">
                           <td>
                             <!-- Avatar -->
                             <div class="avatar avatar-xs align-middle me-2">
@@ -210,7 +218,7 @@
                             </div>
                           </td>
                         </tr>
-                      </tbody>
+                      </paginate>
                     </table>
                   </div>
                   <div class="card-footer d-flex justify-content-between">
@@ -219,70 +227,46 @@
                       class="list-pagination-prev pagination pagination-tabs card-pagination"
                     >
                       <li class="page-item">
-                        <a class="page-link ps-0 pe-4 border-end" href="#">
+                        <a
+                          class="page-link ps-0 pe-4 border-end"
+                          @click="goPrev"
+                          style="cursor: pointer"
+                        >
                           <i class="fe fe-arrow-left me-1"></i> Prev
                         </a>
                       </li>
                     </ul>
 
                     <!-- Pagination -->
-                    <ul
-                      class="list-pagination pagination pagination-tabs card-pagination"
-                    ></ul>
+
+                    <paginate-links
+                      @change="onLangsPageChange"
+                      for="users"
+                      :classes="{
+                        ul: [
+                          'list-pagination',
+                          'pagination',
+                          'pagination-tabs',
+                          'card-pagination',
+                        ],
+                        a: ['page'],
+                      }"
+                    ></paginate-links>
 
                     <!-- Pagination (next) -->
                     <ul
                       class="list-pagination-next pagination pagination-tabs card-pagination"
                     >
                       <li class="page-item">
-                        <a class="page-link ps-4 pe-0 border-start" href="#">
+                        <a
+                          class="page-link ps-4 pe-0 border-start"
+                          @click="goNext"
+                          style="cursor: pointer"
+                        >
                           Next <i class="fe fe-arrow-right ms-1"></i>
                         </a>
                       </li>
                     </ul>
-
-                    <!-- Alert -->
-                    <div
-                      class="list-alert alert alert-dark alert-dismissible border fade"
-                      role="alert"
-                    >
-                      <!-- Content -->
-                      <div class="row align-items-center">
-                        <div class="col">
-                          <!-- Checkbox -->
-                          <div class="form-check">
-                            <input
-                              class="form-check-input"
-                              id="listAlertCheckbox"
-                              type="checkbox"
-                              checked
-                              disabled
-                            />
-                            <label
-                              class="form-check-label text-white"
-                              for="listAlertCheckbox"
-                            >
-                              <span class="list-alert-count">0</span> deal(s)
-                            </label>
-                          </div>
-                        </div>
-                        <div class="col-auto me-n3">
-                          <!-- Button -->
-                          <button class="btn btn-sm btn-white-20">Edit</button>
-
-                          <!-- Button -->
-                          <button class="btn btn-sm btn-white-20">Delete</button>
-                        </div>
-                      </div>
-                      <!-- / .row -->
-
-                      <!-- Close -->
-                      <button
-                        type="button"
-                        class="list-alert-close btn-close"
-                        aria-label="Close"
-                      ></button>
-                    </div>
                   </div>
                 </div>
               </div>
@@ -305,6 +289,9 @@ export default {
       spinner: {
         loading: false,
       },
+      paginate: ['users'],
+      currentPage: 1,
+      perPage: 2,
     };
   },
   methods: {
@@ -323,6 +310,24 @@ export default {
         .finally(() => {
           this.spinner.loading = false;
         });
+    },
+
+    onLangsPageChange(toPage, fromPage) {
+      this.currentPage = toPage;
+    },
+    goPrev() {
+      if (this.currentPage >= 2) {
+        return this.$refs.users.goToPage(this.currentPage--);
+      } else {
+        return this.$refs.users.goToPage(1);
+      }
+    },
+    goNext() {
+      if (this.currentPage <= Math.ceil(this.users.length / this.perPage)) {
+        return this.$refs.users.goToPage(this.currentPage++);
+      } else {
+        return this.$refs.users.goToPage(Math.ceil(this.users.length / this.perPage));
+      }
     },
   },
   created() {
